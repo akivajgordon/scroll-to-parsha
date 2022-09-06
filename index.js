d3bb8e0fc1d99a0d71a7b542e40f576db4c3da11
ezra.gordon <ezra.gordon@nexog.com>
2022-09-07 00:55:42 +0300

    change logic to (re)calculate result whenever a change is made to the selected parsha or image

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
     results: 'RESULTS'
   }
   const state = {
-    step: STEPS.selectParsha
+    step: STEPS.selectParsha,
+    pageFromImage: 0,
+    selectedParshaIndex: 0
   }
 
   const setStep = step => {
@@ -51,119 +53,35 @@
       .classList.add('fade-in-up')
   }
 
-  const animateMessageForUploadPictureButton = message => {
+  const animateMissingInfoMessage = (messageContainer, message, animation) => {
     const upload = document.querySelector('#upload-picture')
 
     upload.style.display = 'initial'
-    document.body.style.justifyContent = 'flex-end'
-    message.style.marginBottom = `calc(0.5rem + ${upload.offsetHeight}px)`
-    message.classList.add('fade-in-up')
+    document.body.style.justifyContent = 'center'
+    messageContainer.innerHTML = message
+    messageContainer.classList.add(animation)
   }
 
-  const startUploadPictureAnimation = (messageContainer, message) => {
+  const renderMissingInfoMessage = (messageContainer, message, animation) => {
     messageContainer.addEventListener(
       'animationend',
       () => {
-        messageContainer.classList.remove('fade-in-up')
+        messageContainer.classList.remove(animation)
         animateUploadPictureButton()
       }
     )
 
-    messageContainer.innerHTML = message
-
-    animateMessageForUploadPictureButton(messageContainer)
+    animateMissingInfoMessage(messageContainer, message, animation)
   }
 
-  const onSelectedParshaChange = e => {
-    const previousStep = getStep()
-
-    // already here, no need to re-animate
-    if (previousStep === STEPS.uploadPicture) return
-
-    setStep(STEPS.uploadPicture)
-
-    const message = `<p>Now take a picture of the open Torah
-      scroll so we know what you're looking at.</p>`
-
-    const messageContainer = document.querySelector('#message')
-
-    if (previousStep === STEPS.selectParsha) {
-      const selectWrapper = e.target.parentNode
-
-      selectWrapper.addEventListener('animationend', e => {
-        messageContainer.classList.remove('fade-out-up')
-        startUploadPictureAnimation(messageContainer, message)
-      })
-
-      messageContainer.addEventListener('animationend', e => {
-        selectWrapper.classList.add('move-to-top')
-      })
-
-      messageContainer.classList.add('fade-out-up')
-    } else {
-      startUploadPictureAnimation(messageContainer, message)
-    }
-  }
-
-  select.addEventListener('change', onSelectedParshaChange)
-
-  document.querySelector('#image').addEventListener('change', async (e) => {
-    const files = e.target.files
-    if (!(files || files.length || select.selectedIndex)) return
-
-    setStep(STEPS.results)
-
-    const file = await imageCompression(files[0], {
-      maxSizeMB: 0.5,
-      onProgress: console.log,
-    })
-
-    const messageContainer = document.querySelector('#message')
-
-    // update and move message back up to center
-    messageContainer.firstChild.addEventListener('animationend', e => {
-      document.body.style.justifyContent = 'center'
-      messageContainer.style.marginBottom = '0'
-
-      messageContainer.innerHTML = `<h2 class="fade-in-down"
-      style="text-align: center;">Analyzing...</h2>`
-    })
-
-    messageContainer.firstChild.classList.add('fade-out-down')
-
-    const {
-      data: { text },
-    } = await worker.recognize(file)
-
-    const scores = pages.map((pageText) => {
-      return stringSimilarity.compareTwoStrings(
-        text.replace(/[^א-ת]/g, ''),
-        pageText
-      )
-    })
-
-    const highestScore = Math.max(...scores)
-
-    const pageWithHighestScore = scores.findIndex(
-      (score) => score === highestScore
-    )
-
-    console.log({ scores, highestScore, pageWithHighestScore })
-    console.log({
-      scores: scores
-        .map((score, index) => ({ score, page: index + 1 }))
-        .sort((a, b) => (a.score - b.score > 0 ? -1 : 1)),
-    })
-
-    const startPageOfSelectedParsha = parshiyot[select.selectedIndex - 1].startPage
-
-    const pageFromImage = pageWithHighestScore + 1
+  const resultMessage = (selectedParshaIndex, pageFromImage) => {
+    const startPageOfSelectedParsha = parshiyot[selectedParshaIndex].startPage
 
     const columnsToScroll = startPageOfSelectedParsha - pageFromImage
 
     const needsToAdvance = columnsToScroll > 0
 
-    const message =
+    return (
       columnsToScroll === 0
         ? `<h2 style="text-align: center;">You're already there!</h2>
           <p>Feel free to take another picture to see if you need to make any
@@ -185,6 +103,13 @@
           get to column ${startPageOfSelectedParsha}.</p>
           <p>When you get there, feel free to take another
           picture to see if you need to make any adjustments.</p>`
+    )
+  }
+
+  const renderResult = () => {
+    const messageContainer = document.querySelector('#message')
+
+    const message = resultMessage(state.selectedParshaIndex - 1, state.pageFromImage)
 
     messageContainer.firstChild.addEventListener('animationend', e => {
       messageContainer.innerHTML = message
@@ -194,5 +119,108 @@
     })
 
     messageContainer.firstChild.classList.add('fade-out-down')
+  }
+
+  const renderSelectParsha = () => {
+    const message = `<p>It looks like you're on column ${state.pageFromImage}.</p>
+      <p>Select a parsha so we know where you need to scroll to.</p>`
+
+    renderMissingInfoMessage(document.querySelector('#message'), message, 'fade-in-down')
+  }
+
+  const renderUploadPicture = () => {
+    const message = `<p>Now take a picture of the open Torah
+    scroll so we know what you're looking at.</p>`
+    const messageContainer = document.querySelector('#message')
+    const selectWrapper = document.querySelector('#select-parsha').parentNode
+
+    selectWrapper.addEventListener('animationend', e => {
+      messageContainer.classList.remove('fade-out-up')
+      renderMissingInfoMessage(messageContainer, message, 'fade-in-up')
+    })
+
+    messageContainer.addEventListener('animationend', e => {
+      selectWrapper.classList.add('move-to-top')
+    })
+
+    messageContainer.classList.add('fade-out-up')
+  }
+
+  const onSelectedParshaChange = e => {
+    state.selectedParshaIndex = e.target.selectedIndex
+
+    const previousStep = getStep()
+    // already here, no need to re-animate
+    if (previousStep === STEPS.uploadPicture && !state.pageFromImage) return
+
+    setStep(STEPS.uploadPicture)
+
+    if (state.pageFromImage) {
+      if (state.selectedParshaIndex) {
+        renderResult()
+      } else {
+        renderSelectParsha()
+      }
+    } else if (state.selectedParshaIndex) {
+      renderUploadPicture()
+    }
+  }
+
+  select.addEventListener('change', onSelectedParshaChange)
+
+  const renderAnalyzePicture = () => {
+    const messageContainer = document.querySelector('#message')
+
+    // update and move message back up to center
+    messageContainer.firstChild.addEventListener('animationend', e => {
+      document.body.style.justifyContent = 'center'
+      messageContainer.style.marginBottom = '0'
+
+      messageContainer.innerHTML = `<h2 class="fade-in-down"
+        style="text-align: center;">Analyzing the picture...</h2>`
+    })
+
+    messageContainer.firstChild.classList.add('fade-out-down')
+  }
+
+  document.querySelector('#image').addEventListener('change', async (e) => {
+    const files = e.target.files
+    if (!(files || files.length)) return
+
+    setStep(STEPS.results)
+
+    const file = await imageCompression(files[0], {
+      maxSizeMB: 0.5,
+      onProgress: console.log,
+    })
+
+    renderAnalyzePicture()
+
+    const {
+      data: { text },
+    } = await worker.recognize(file)
+
+    const scores = pages.map((pageText) => {
+      return stringSimilarity.compareTwoStrings(
+        text.replace(/[^א-ת]/g, ''),
+        pageText
+      )
+    })
+
+    const highestScore = Math.max(...scores)
+
+    const pageWithHighestScore = scores.findIndex(
+      (score) => score === highestScore
+    )
+
+    state.pageFromImage = pageWithHighestScore + 1
+
+    if (state.pageFromImage) {
+      if (state.selectedParshaIndex) {
+        renderResult()
+      } else {
+        renderSelectParsha()
+      }
+    }
   })
 })()
